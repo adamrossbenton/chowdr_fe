bfedac8a19ff2e5f0766ed7e6ef5346c5d45977c
Adam Ross Benton <adamrossbenton@gmail.com>
2021-11-20 10:57:56 -0800

    add states and handleChange to login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,44 @@
-import React from "react"
+import React, {useState} from "react"
+import PropTypes from "prop-types"
 
-const Login = props => {
+const Login = ({setToken}) => {
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
+    const [newForm, setNewForm] = useState()
+
+    const handleChange = e => {
+        setNewForm({
+            ...newForm,
+            [e.target.name]: e.target.value
+        })
+    }
+    
     return <>
         <h1>Enter username and password to log in</h1>
         <form>
             <label>
                 <p>Username: </p>
-                <input type="text" placeholder="Username"/>
+                <input 
+                    type="text" 
+                    placeholder="Username" 
+                    onChange={handleChange}
+                    />
             </label>
             <label>
                 <p>Password: </p>
-                <input type="password" placeholder="Password"/>
+                <input 
+                    type="password" 
+                    placeholder="Password"
+                    onChange={handleChange}
+                    />
             </label>
             <button type="submit">Login</button>
         </form>
     </>
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
 }
 
 export default Login
