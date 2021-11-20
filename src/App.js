b6ae981b5f516cbce54237f67fe6e38d8e743598
Adam Ross Benton <adamrossbenton@gmail.com>
2021-11-20 09:40:18 -0800

    setting up routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {Route, Switch} from "react-router-dom"
+
+import Main from "./components/Main"
+import Show from "./components/Show"
 
 function App() {
   return (
     <div className="App">
       <h1>hello chowdr</h1>
+      <Switch>
+        <Route path="/chowders">
+          <Main/>
+        </Route>
+        <Route path="/chowders/:id">
+          <Show/>
+        </Route>
+      </Switch>
     </div>
   );
 }
