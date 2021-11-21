2feee5072ad3505e1e70525ad7bbf4761408436d
Adam Ross Benton <adamrossbenton@gmail.com>
2021-11-20 16:24:59 -0800

    added login button if not logged in

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
-// import {Route, Switch} from "react-router-dom"
 
 import Header from './components/Header';
 import Main from './components/Main';
-import Login from "./components/Login"
 
 import useToken from "./hooks/useToken"
 
 function App() {
 
   const {token, setToken} = useToken()
-
-  // if (!token) {
-  //   return <div className="App">
-  //     <Header />
-  //     <Login setToken={setToken} />
-  //   </div>
-  // }
 
   return (
     <div className="App">
