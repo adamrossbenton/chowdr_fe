7f69e88e1f4c0d30c4f5dbc329c51492c095a13d
Adam Ross Benton <adamrossbenton@gmail.com>
2021-11-23 14:41:55 -0800

    create button is now user locked

--- src/components/Main.js
+++ src/components/Main.js
@@ -57,7 +57,7 @@ const deleteChowders = async id => {
         <main>
             <Switch>
                 <Route exact path="/">
-                    <Index chowders={chowders} createChowders={createChowders} />
+                    <Index chowders={chowders} createChowders={createChowders} token={props.token} />
                 </Route>
                 <Route 
                 path="/chowders/:id"
