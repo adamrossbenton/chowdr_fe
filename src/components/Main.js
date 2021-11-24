487bb1ff3e8b7452d2abedc65d62db22c82d6c50
Adam Ross Benton <adamrossbenton@gmail.com>
2021-11-23 16:08:09 -0800

    working on create user talking to be

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ function Main(props){
     const [chowders, setChowders] = useState(null);
 
     const URL = "https://chowdr-app.herokuapp.com/chowders/";
-
+    
     const getChowders = async () => {
         const response = await fetch(URL);
         const data = await response.json();
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,7 +5,28 @@ const Signup = props => {
     const [newForm, setNewForm] = useState()
 
     const history = useHistory()
-    const userUrl = "https://chowdr-app.herokuapp.com/user/login"
+    // const userUrl = "https://chowdr-app.herokuapp.com/user/signup"
+    const userUrl = "http://localhost:4000/user/signup"
+
+    const createUser = async creds => {
+        await fetch(userUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(creds),
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        createUser(newForm)
+        setNewForm({
+            username: "",
+            password: "",
+        })
+        history.push("/login")
+    }
 
     const handleChange = e => {
         setNewForm({
@@ -15,6 +36,23 @@ const Signup = props => {
     }
     
     return <>
+        <section>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                />
+                <input type="submit" value="Sign up"/>
+            </form>
+        </section>
         <Link to="/login"><button>Cancel</button></Link>
     </>
 }
