0ea82c85984949152d746fe97cf27be44145c900
Adam Ross Benton <adamrossbenton@gmail.com>
2021-11-29 23:08:50 -0800

    saving before pulling dev changes

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,32 +1,46 @@
-import React from "react"
+import React, {useState} from "react"
 import {useHistory} from "react-router-dom"
 
 const User = props => {
-    
     const token = props.token
     const history = useHistory()
+    const allChowders = props.chowders
 
-    console.log(props.chowders)
-    console.log(token.toString())
+    const [info, setInfo] = useState(allChowders)
+
+    console.log(allChowders)
+    console.log(token)
     const tokenTest = () => token === "adamrossbenton"
     console.log(tokenTest())
 
-    // const userChowders = async () => {
-    //     const newArray = await props.chowders.filter(chow =>
-    //         chow.creator === token
-    //     )
-    //     return await newArray
-    // }
-
-    // console.log(userChowders())
-
+    // if not logged in
     if (!token) {
         history.push("/user/login")
     }
 
-    return <>
-        <h1>hello {token}</h1>
-    </>
+    // if loaded
+    const loaded = () => {
+        const asyncChowders = async arr => {
+            await arr.reduce(async (acc, nxt) => {
+                const userMatch = chowder => chowder.creator === token
+                const res = await userMatch(nxt)
+                if (!res) {
+                    return acc
+                }
+    
+                return (await acc).concat(nxt)
+            }, [])
+        }
+    
+        const userChowders = asyncChowders(allChowders)    
+    }
+
+    // if loading
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
+
+    return allChowders ? loaded() : loading()
 }
 
 export default User
